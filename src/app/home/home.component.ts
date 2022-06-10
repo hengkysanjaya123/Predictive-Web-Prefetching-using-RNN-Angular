04254ed073c5504414aaba21b9d8ff6641f19d5a
hengkysanjaya123 <hengkysanjaya204@gmail.com>
2022-06-10 23:03:25 +0700

    Add search functionality for paths

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,23 @@ import { Path, PathService } from '../../services/path/path.service';
 export class HomeComponent implements OnInit {
 
   paths: Path[];
+  filteredPaths: Path[];
 
   constructor(private pathService: PathService) {
   }
 
   ngOnInit(): void {
     this.paths = this.pathService.getPaths();
+    this.filteredPaths = this.paths;
+  }
+
+  search(value: string): void {
+    value = value.trim();
+    value = value.toLowerCase();
+    if (value === '') {
+      this.filteredPaths = this.paths;
+    } else {
+      this.filteredPaths = this.paths.filter(p => p.name != null && p.name.toLowerCase().startsWith(value));
+    }
   }
 }
