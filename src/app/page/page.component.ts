1524fd1772ba1f146acbfedc08f1bf7e431ed16d
hengkysanjaya123 <hengkysanjaya204@gmail.com>
2022-07-02 15:22:07 +0700

    Add ability to disable the prefetching

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Path, PathService } from '../../services/path/path.service';
 import { CountryService, Location } from '../../services/country/country.service';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-page',
@@ -24,7 +25,10 @@ export class PageComponent implements OnInit {
     navigator.serviceWorker.ready.then(registration => {
       this._serviceWorker = registration.active;
     });
-    this.route.params.subscribe(({path}) => {
+    combineLatest([
+      this.route.queryParams,
+      this.route.params
+    ]).subscribe(([{prefetch}, {path}]) => {
       this.currentPath = this.pathService.get(path);
 
       const pathSequence = sessionStorage.getItem('path');
@@ -34,12 +38,14 @@ export class PageComponent implements OnInit {
         sessionStorage.setItem('path', this.currentPath.originalPath + ',');
       }
 
-      if (this._serviceWorker) {
+      if (this._serviceWorker && prefetch !== 'false') {
         this._serviceWorker.postMessage({
           path: this.currentPath,
           country: this.currentLocation.country,
           pathSequence: sessionStorage.getItem('path')
         });
+      } else {
+        console.log('prefetching disabled');
       }
     });
   }
